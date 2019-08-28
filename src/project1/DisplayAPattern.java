1a734736faa8b92883e8fea2df6ad125bce366d5
tmax818 <tylermaxwell661@gmail.com>
2019-08-28 13:16:03 -0700

    init commit

---
+++ src/project1/DisplayAPattern.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project1;
+
+/**
+ *
+ * @author Your Name
+ */
+public class DisplayAPattern {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("     J    A   V     V    A");
+        System.out.println("     J   A A   V   V    A A ");
+        System.out.println(" J   J  AAAAA   V V    AAAAA ");
+        System.out.println("  J J  A     A   V    A     A ");
+    }
+    
+}
