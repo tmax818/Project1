ac76988e45590522cbf945617a8a44b9b353b488
tmax818 <tylermaxwell661@gmail.com>
2019-09-09 11:11:28 -0700

    complete assignment

--- src/project1/MyInitials.java
+++ src/project1/MyInitials.java
@@ -5,14 +5,19 @@ package project1;
  * Source Code File: MyInitials.java
  * Programmer: Tyler Maxwell
  * Due: 9/9/19
- * Description: Part 2 of Project 1. A program that calculates and displays the perimeter and area of a circle with a radius of 5.5.
+ * Description: Part 2 of Project 1. A program that prints my initals in a prescribed format.
  */
 
 public class MyInitials {
 
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("TTTTTTTT     MMM   MMM");
+        System.out.println("   TT        M  M M  M");
+        System.out.println("   TT        M   M   M");
+        System.out.println("   TT        M       M");
+        System.out.println("   TT        M       M");
+        System.out.println("   TT        M       M");
     }
     
 }
